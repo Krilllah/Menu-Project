52055fa742e47324a63a0c7ce2a5f43b966ecfb2
kandreev2023 <90445644+Krilllah@users.noreply.github.com>
2021-12-23 14:59:01 +0300

    saving file

--- Restaurant.py
+++ Restaurant.py
@@ -1,8 +1,11 @@
 from unicodedata import category
 from Dish import Dish
 from Menu import Menu
+from Ingredient_Repository import Ingredient_Repo
+from Ingredients import Ingredient
 
 menu = Menu()
+storage = Ingredient_Repo()
 file1 = open('menu.txt', 'r')
 if file1 is not None:
     for line in file1.readlines():
@@ -18,8 +21,14 @@ while True:
     if command == 'exit':
         print("Thank you, see you")
         file1 = open('menu.txt', 'w+')
+        for i in storage.ingredients:
+            
         for i in menu.dishes:
-            file1.write(f'{i.name},{i.price},{i.mass},{i.category},\n')
+            comfy_content_zero = []
+            for j in i.content:
+                comfy_content_zero.append(f'{".".join(j[0])};{j[1]}')
+            comfy_content = ', '.join(comfy_content_zero)
+            file1.write(f'{i.name},{i.price},{i.mass},{i.category},{comfy_content}\n')
         file1.close()
         break
     elif command == 'add':
@@ -31,7 +40,11 @@ while True:
         mass = int(input())
         print("Enter category:")
         category = input()
-        dish = Dish(name, price, mass, category)
+        content = input("Enter content:\n").split(', ')
+        dish = Dish(name, price, mass, category, [[Ingredient(i.split(';')[0].split('.')[0],
+                                                              i.split(';')[0].split('.')[1],
+                                                              i.split(';')[0].split('.')[2]),
+                                                   i.split(';')[1]] for i in content])
         menu.addDish(dish)
     elif command == 'remove':
         print("Enter name:")
@@ -41,7 +54,7 @@ while True:
         menu.printMenu()
     elif command == 'gimme':
         name = input("Enter name:\n")
-        the_dish = menu.check_dish_existance(name)
+        the_dish = menu.check_dish_existance(name, storage)
         if the_dish is not None:
             print(f'''Your dish:
         & {str(the_dish.name).capitalize()} &
@@ -49,5 +62,19 @@ while True:
         mass: {the_dish.mass}гр
         category: {the_dish.category}
 Enjoy your meal!''')
+    elif command == 'replenish':
+        name = input("Enter name:\n")
+        the_ingredient = storage.check_ingredient_existance(name)
+        if the_ingredient is not None:
+            amount = input("Enter amount:\n")
+            storage.ingredients[the_ingredient] += amount
+        else:
+            calories = input("Enter calories:\n")
+            price = input("Enter price:\n")
+            amount = input("Enter amount:\n")
+            the_ingredient = Ingredient(name, calories, price)
+            storage.ingredients[the_ingredient] = amount
+    elif command == 'check storage':
+        storage.printIngredient_Repo()
     else:
         print("Sorry, man. I don't have this command")
